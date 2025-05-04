57a27ac61911ac5eaf61432f38456e710c96a54c
SnehalSura <snehalsura@gmail.com>
2025-05-04 20:24:35 +0530

    Added completed field in models which was missing

--- tasks/models.py
+++ tasks/models.py
@@ -5,6 +5,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField(default=date.today)  # Allows updates if needed using patch method
+    completed = models.BooleanField(default=False, blank=True, null=True)
     
     # date = models.DateField(auto_now_add=True)   # Automatically set to the current date when a new task is created, but
     # prevents it from being updated in the future. So, used default=date.today property
